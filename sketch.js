883915272ccc0f214b9241b1af2457001f06a24d
Vidhita565 <73418284+Vidhita565@users.noreply.github.com>
2021-01-10 18:21:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ var food, database
 
 function preload()
 {
-  dogImg = loadImage("images/dogImg.png");
-  dogImg1 = loadImage("images/dogImg1.png");
+  dogImg = loadImage("dogImg.png");
+  dogImg1 = loadImage("dogImg1.png");
  
 }
 
